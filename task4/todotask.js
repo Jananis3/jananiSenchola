43240628f8295bc5c331104ef0b88c6da630aea8
Jananis3 <112616247+Jananis3@users.noreply.github.com>
2023-09-07 16:12:24 +0530

    Update todotask.js

--- task4/todotask.js
+++ task4/todotask.js
@@ -1,4 +1,47 @@
 // todotask.js
+const form = document.querySelector('#form')
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+form.addEventListener('submit',(e)=>{
+   
+    if(!validateInputs()){
+        e.preventDefault();
+    }
+})
+
+function validateInputs(){
+    const usernameVal = username.value.trim()
+    const emailVal = email.value.trim();
+    let success = true
+
+    if(usernameVal===''){
+        success=false;
+        setError(username,'Username is required')
+    }
+    else{
+        setSuccess(username)
+    }
+
+    if(emailVal===''){
+        success = false;
+        setError(email,'Email is required')
+    }
+    else if(!validateEmail(emailVal)){
+        success = false;
+        setError(email,'Please enter a valid email')
+    }
+    else{
+        setSuccess(email)
+    }
+}
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
 
 window.onload = () => {
 	const form1 = document.querySelector("#addForm");
