cb0f8027a597c27dcdb763c5ba8e0e61409938cf
Jananis3 <112616247+Jananis3@users.noreply.github.com>
2023-09-09 07:24:13 +0530

    Add files via upload

---
+++ task5/sample.js
@@ -0,0 +1,6 @@
+var myoffcanvas = document.getElementById('myoffcanvas');
+var bsoffcanvas = new bootsrap.myoffcanvas(myoffcanvas);
+myoffcanvas.addEventListener('hidden.bsoffcanvas' , funtion()
+{
+	alert("add item")
+});
